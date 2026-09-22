7e233ff7e5cb938d2a3ef467451161c647eefbac
nourmahran <nour.nayer2008@gmail.com>
2026-09-22 11:05:28 +0300

    Filter todos by done query parameter

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -2,7 +2,13 @@ const Todo = require('../models/Todo');
 
 const getTodos = async (req, res) => {
   try {
-    const todos = await Todo.find().sort({ createdAt: -1 });
+    const filter = {};
+    if (req.query.done === 'true') {
+      filter.done = true;
+    } else if (req.query.done === 'false') {
+      filter.done = false;
+    }
+    const todos = await Todo.find(filter).sort({ createdAt: -1 });
     res.json(todos);
   } catch (err) {
     res.status(500).json({ error: 'Server error' });
