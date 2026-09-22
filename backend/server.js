60d2826e76d8f98dd3059cc10b262a3484df8eef
nourmahran <nour.nayer2008@gmail.com>
2026-09-22 11:04:47 +0300

    Fix routes import capitalization

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const todoRoutes = require('./Routes/todoRoutes')
+const todoRoutes = require('./routes/todoRoutes');
 
 const app = express();
 app.use(cors({
